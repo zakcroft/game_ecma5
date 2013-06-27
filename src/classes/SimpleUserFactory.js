05afd82948a696c4fb16ddcd2647f1984d85c699
zakcorft <zakcroft@gmail.com>
2013-06-27 17:19:58 +0100

    added part of first decorator and changed factory function to call decorators

--- src/classes/SimpleUserFactory.js
+++ src/classes/SimpleUserFactory.js
@@ -1,5 +1,5 @@
 App.classes.SimpleAvatarFactory = (function (classes, decorators) {
-
+    "use strict"
     function SimpleAvatarFactory() {
 
     };
@@ -12,11 +12,21 @@ App.classes.SimpleAvatarFactory = (function (classes, decorators) {
         // its better to have a settings object to pass up the hierarchy as easily scaled for
         // adding new parameters at any level.
 
-        createAvatar: function (type, settings, decoration) {
+        createAvatar: function (type, settings, decorations) {
 
             var avatar = new SimpleAvatarFactory.avatars[type](settings);
 
+            // Iterate through the options and instantiate decorators.
+            for (var i = 0, len = decorations.length; i < len; i++) {
+                var decorator = SimpleAvatarFactory.decorators[decorations[i]];
+                if (typeof decorator !== 'function') {
+                    throw new Error('Decorator ' + decorations[i] + ' not found.');
+                }
 
+                avatar = new decorator(avatar, decorations[i]);
+            }
+
+            // Check the interface and return the finished object.
             App.base.Interface.ensureImplements(avatar, App.interfaces.user);
 
             return avatar;
@@ -34,7 +44,7 @@ App.classes.SimpleAvatarFactory = (function (classes, decorators) {
     // Decorators name to decorator class name mapping.
     SimpleAvatarFactory.decorators = {
 
-          'master': decorators.masterDecorators
+          'master': decorators.MasterDecorator
 //        'spirit_invoker': SpiritInvoker,
 //        'tree_talker': TreeTalker,
 //        'teleport': Teleport
